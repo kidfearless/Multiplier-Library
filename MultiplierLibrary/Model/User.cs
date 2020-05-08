a0f6cf4862dd1371bbed71271672e26fe1c241af
KiD Fearless <kidfearless1@gmail.com>
2020-05-08 13:43:06 -0600

    remove bad sqlite attributes

--- MultiplierLibrary/Model/User.cs
+++ MultiplierLibrary/Model/User.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
-using SQLite;
 
 namespace MultiplierLibrary.Model
 {
 	class User
 	{
-		[PrimaryKey, AutoIncrement]
 		public int ID { get; set; }
 		public string UserName { get; set; }
 		public int Score { get; set; }
